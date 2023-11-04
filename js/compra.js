04c79805b915443caaa04f91c7a869f6f5524d0f
Sony <sony.montoya.12@gmail.com>
2023-11-04 12:54:31 -0500

    [ADD] EDICION DE LA DIRECCION

--- js/compra.js
+++ js/compra.js
@@ -1,10 +1,17 @@
 const DOMEvents = {
     init() {
         this.btnPendientePago();
+        this.btnEditarDireccion();
     },
     btnPendientePago() {
         document.getElementById('btnPendientePago').addEventListener('click', function () {
             listarPendientePago();
+        });
+    },
+    btnEditarDireccion() {
+        document.getElementById('btnEditarDireccion').addEventListener('click', function () {
+            const id = this.getAttribute('data-id');
+            editarDireccionExecute(id);
         });
     }
 }
@@ -19,9 +26,11 @@ const HttpRequest = {
                     'Content-Type': 'application/json'
                 },
                 body: JSON.stringify({
-                    procedure: '{ CALL SP_FW_GESTION_COMPRAS(?,?)}', // 2 parametros
+                    procedure: '{ CALL SP_FW_GESTION_COMPRAS(?,?,?,?)}', // 4 parametros
                     params: [json['accion'],
-                        json['estado'] ?? 0
+                        json['id'] ?? 0,
+                        json['estado'] ?? 0,
+                        json['direccion'] ?? ''
                     ]
                 })
             })
@@ -41,7 +50,6 @@ const HttpRequest = {
         });
     }
 }
-
 
 function listarPendientePago() {
     const json = {
@@ -63,15 +71,15 @@ function listarPendientePago() {
                                     <img class="compra-image" src="${x.img}" alt="">
                                 </span>
                                 <span class="compra-titulo col-md-3">${x.nombre}</span>
-                                <span class="compra-precio col-md-1 text-center">S/${x.precio}</span>
+                                <span class="compra-precio col-md-1 text-center">S/ ${x.precio}</span>
                                 <span class="compra-cantidad col-md-2 text-center">${x.cantidad} unidad(es)</span>
                                 <span class="compra-direccion col-md-2 text-center">${x.direccion}</span>
                                 <span class="compra-fecha col-md-2 text-center">${x.fecha}</span>
                                 <span class="content-compra-accion col-md-1">
-                                    <a href="#" class="compra-action">
+                                    <a id="btnEditar_${x.id}" href="#" class="compra-action-editar-penPago">
                                         <i class="fa fa-pencil" aria-hidden="true"></i>
                                     </a>
-                                    <a href="#" class="compra-action">
+                                    <a id="btnEliminar_${x.id}" href="#" class="compra-action-eliminar-penPago">
                                         <i class="fa fa-trash" aria-hidden="true"></i>
                                     </a>
                                 </span>
@@ -80,11 +88,75 @@ function listarPendientePago() {
                 });
                 body += `</div>`;
                 section.innerHTML = body;
+
+                // Edicion
+                const arrayEditar = document.getElementsByClassName('compra-action-editar-penPago');
+                Array.from(arrayEditar).forEach(x => {
+                    x.addEventListener('click', function () {
+                        const id = parseInt(this.id.split('_')[1]);
+                        modalEditarPenPago(id);
+                    });
+                });
+
+                // Eliminacion
+                const arrayEliminar = document.getElementsByClassName('compra-action-eliminar-penPago');
+                Array.from(arrayEliminar).forEach(x => {
+                    x.addEventListener('click', function () {
+                        const id = this.id.split('_')[1];
+                        eliminarCompra(id);
+                    });
+                });
             }
         });
     } catch (error) {
         console.log(error);
     }
+}
+
+function modalEditarPenPago(id) {
+    const btnEditar = document.getElementById('btnEditarDireccion');
+    btnEditar.setAttribute('data-id', id);
+    const json = {
+        accion: 2,
+        id: id
+    }
+
+    try {
+        HttpRequest.execProcGestionCompras(json).then(response => {
+            if (response.status) {
+                const data = response.data[0];
+                document.getElementById('titleProductoDireccion').textContent = data.nombre;
+                document.getElementById('imgProductoDireccion').src = data.img;
+            }
+        });
+
+    } catch (error) {
+        console.log(error);
+    } finally {
+        $('#modalEditarDireccion').modal('show');
+    }
+}
+
+function editarDireccionExecute(id) {
+    const json = {
+        accion: 3,
+        id: id,
+        direccion: document.getElementById('txtDireccion').value
+    }
+
+    HttpRequest.execProcGestionCompras(json).then(response => {
+        Swal.fire({
+            title: '¡Éxito!',
+            text: 'Se modificó la dirección con éxito.',
+            icon: 'success',
+            confirmButtonText: 'Aceptar'
+        }).then((result) => {
+            if (result.isConfirmed) {
+                $('#modalEditarDireccion').modal('hide');
+                listarPendientePago();
+            }
+        });
+    });
 }
 
 document.addEventListener('DOMContentLoaded', function () {
